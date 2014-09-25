fd144d613dc51a23353285b4e7e84b03cb965eb5
Aaron Williamson <aaron@copiesofcopies.org>
2014-09-24 21:16:13 -0400

    Moved main to accounts and buddylist to main.

---
+++ accounts.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"reflect"
+	"regexp"
+	"strings"
+	"github.com/andlabs/ui"
+//	"github.com/agl/xmpp"
+)
+
+type DisplayAccount struct {
+	Enabled bool
+	Name    string
+}
+
+func listAccounts() {
+	// Import the configuration
+	config := importConfig();
+
+	// Assemble table of accounts
+	accounts := make([]DisplayAccount, len(config.Accounts))
+
+	// Pull accounts from config file
+	for i, account := range config.Accounts {
+		enabled := account.Enabled
+		acctName := strings.Join([]string{account.Name, account.Domain}, "@")
+		accounts[i] = DisplayAccount{enabled, acctName}
+	}	
+
+	// Populate table
+	accountTable := ui.NewTable(reflect.TypeOf(accounts[0]))
+	accountTable.Lock()
+	e := accountTable.Data().(*[]DisplayAccount)
+	*e = accounts
+	accountTable.Unlock()
+
+	// Assemble button stack
+	addButton := ui.NewButton("Add")
+	modifyButton := ui.NewButton("Modify")
+	deleteButton := ui.NewButton("Delete")
+
+	// Set up channel for sending data between windows
+	done := make(chan Account)
+
+	// When "Delete" is clicked, remove selected account
+	deleteButton.OnClicked(func() {
+		sel := accountTable.Selected()
+		cfgAccts := config.Accounts
+
+		// Delete the account from the config file
+		config.Accounts = append(cfgAccts[:sel], cfgAccts[sel+1:]...)
+		config.Save()
+
+		// Delete the account from the table by building a new slice omitting it
+		accountTable.Lock()
+		tblAccts := accountTable.Data().(*[]DisplayAccount)
+		aa := *tblAccts
+		*tblAccts = append(aa[:sel], aa[sel+1:]...)
+		accountTable.Unlock()
+	})
+
+	// When "Modify" is clicked, present selected account for editing
+	modifyButton.OnClicked(func() {
+		sel := accountTable.Selected()
+		cfgAccts := config.Accounts
+
+		if sel < 0 {
+			return
+		}
+
+		// Retrieve selected account
+		acct := cfgAccts[sel]
+		
+		// Open window to edit selected account
+		editAccount(acct, done)
+
+		go func() {
+			// Get edited account
+			editedAcct := <- done
+
+			// Update the account
+			config.Accounts[sel] = editedAcct
+			config.Save()
+
+			// Update the accounts list
+			accounts := make([]DisplayAccount, len(config.Accounts))
+			
+			for i, account := range config.Accounts {
+				acctName := strings.Join([]string{account.Name, account.Domain}, "@")
+				accounts[i] = DisplayAccount{account.Enabled, acctName}
+			}	
+
+			// Update accountTable
+			accountTable.Lock()
+			e := accountTable.Data().(*[]DisplayAccount)
+			*e = accounts
+			accountTable.Unlock()
+		}()
+	})
+
+	// When "Add" is clicked, open window to enter new account info
+	addButton.OnClicked(func() {
+		// Create an empty Account to pass to editAccount
+		var account Account;
+
+		// Open the edit window and wait for a result
+		editAccount(account, done)
+
+		// Update accounts list concurrently once we hear back from the edit window
+		go func() {
+			acct := <-done
+
+			// Add new account to config.Accounts
+			config.Accounts = append(config.Accounts, acct)
+			config.Save()
+
+			// Update the accounts list
+			accounts := make([]DisplayAccount, len(config.Accounts))
+			
+			for i, account := range config.Accounts {
+				acctName := strings.Join([]string{account.Name, account.Domain}, "@")
+				accounts[i] = DisplayAccount{account.Enabled, acctName}
+			}	
+
+			accountTable.Lock()
+			e := accountTable.Data().(*[]DisplayAccount)
+			*e = accounts
+			accountTable.Unlock()
+		}()
+	})
+
+	// Put the interface together
+	buttons := ui.NewHorizontalStack(
+		addButton,
+		modifyButton,
+		deleteButton)
+
+	accountsInterface := ui.NewVerticalStack(
+		accountTable,
+		buttons)
+
+	accountsInterface.SetStretchy(0)
+
+	// Build/display the window
+	acctWin := ui.NewWindow("Accounts", 400, 500, accountsInterface)
+
+	acctWin.OnClosing(func() bool {
+		ui.Stop()
+		return true
+	})
+
+	acctWin.Show()
+}
+
+func editAccount(account Account, done chan Account) {
+	// Initialize fields
+	enabled := ui.NewCheckbox("Enabled")
+	username := ui.NewTextField()
+	domain := ui.NewTextField()
+	password := ui.NewPasswordField()
+
+	// If this is an existing account, populate the fields from the config file
+	if len(account.Name) > 0 {
+		enabled.SetChecked(account.Enabled)
+		username.SetText(account.Name)
+		domain.SetText(account.Domain)
+		password.SetText(account.Password)		
+	} else {
+		enabled.SetChecked(false)
+		username.SetText("e.g. 'esnowden'")
+		domain.SetText("e.g. 'ccc.de'")
+		password.SetText("password1234")
+	}
+
+	// Assemble input fields
+	textFields := ui.NewVerticalStack(
+		ui.NewStandaloneLabel("Enabled"),
+		enabled,
+		ui.NewStandaloneLabel("Username"),
+		username,
+		ui.NewStandaloneLabel("Domain"),
+		domain,
+		ui.NewStandaloneLabel("Password"),		
+		password)
+
+	// Assemble button stack
+	saveButton := ui.NewButton("Save")
+	cancelButton := ui.NewButton("Cancel")
+
+	buttons := ui.NewHorizontalStack(
+		saveButton,
+		cancelButton)
+
+	editAccountInterface := ui.NewVerticalStack(
+		textFields,
+		buttons)
+
+	editAccountInterface.SetStretchy(0)
+
+	// Build and display window
+	editWin := ui.NewWindow("Edit Account", 400, 400, editAccountInterface)
+
+	editWin.OnClosing(func() bool {
+		return true
+	})
+
+	//Save a new account
+	saveButton.OnClicked(func() {
+		//Validate the data (TODO: use the fields' methods for this)
+		validUsername := regexp.MustCompile("[^\x00-\x20\x22\x26\x27\x2F\x3A\x3C\x3E\x40\x7F]+")
+//		validDomain := regexp.MustCompile("(\w{2,}\.\w{2,3}\.\w{2,3}|\w{2,}\.\w{2,3})$")
+
+		if strings.Trim(username.Text(), " ") == "" {
+			username.Invalid("You must enter a username")
+		} else if validUsername.MatchString(username.Text()) == false {
+			username.Invalid("Username contains invalid characters")
+//		} else if validDomain.MatchString(domain.Text()) == false {
+//			domain.Invalid("Domain contains invalid characters")
+		} else {
+			//Instantiate a new account and grow config.Accounts to add it
+			acct := Account{Enabled: enabled.Checked(), Name: username.Text(), Domain: domain.Text(), Password: password.Text()}
+			editWin.Hide()
+			done <- acct
+		}
+	})
+
+	cancelButton.OnClicked(func() {
+		editWin.Hide()
+	})
+
+	editWin.Show()
+}
+
+func main() {
+	go ui.Do(listAccounts)
+	err := ui.Go()
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,240 +1,250 @@
 package main
 
 import (
-	"reflect"
-	"regexp"
-	"strings"
-	"github.com/andlabs/ui"
-//	"github.com/agl/xmpp"
+	"bytes"
+	"encoding/hex"
+//	"encoding/xml"
+//	"errors"
+//	"flag"
+	"fmt"
+	"io"
+//	"io/ioutil"
+	"net/url"
+	"os"
+//	"os/exec"
+//	"os/signal"
+//	"path/filepath"
+//	"strconv"
+//	"strings"
+	"sync"
+//	"syscall"
+	"time"
+
+	"code.google.com/p/go.crypto/otr"
+	"code.google.com/p/go.crypto/ssh/terminal"
+	"code.google.com/p/go.net/html"
+	"code.google.com/p/go.net/proxy"
+	"github.com/agl/xmpp"
 )
 
-type DisplayAccount struct {
-	Enabled bool
-	Name    string
-}
+// OTRWhitespaceTagStart may be appended to plaintext messages to signal to the
+// remote client that we support OTR. It should be followed by one of the
+// version specific tags, below. See "Tagged plaintext messages" in
+// http://www.cypherpunks.ca/otr/Protocol-v3-4.0.0.html.
+var OTRWhitespaceTagStart = []byte("\x20\x09\x20\x20\x09\x09\x09\x09\x20\x09\x20\x09\x20\x09\x20\x20")
 
-func listAccounts() {
-	// Import the configuration
-	config := importConfig();
+var OTRWhiteSpaceTagV1 = []byte("\x20\x09\x20\x09\x20\x20\x09\x20")
+var OTRWhiteSpaceTagV2 = []byte("\x20\x20\x09\x09\x20\x20\x09\x20")
+var OTRWhiteSpaceTagV3 = []byte("\x20\x20\x09\x09\x20\x20\x09\x09")
 
-	// Assemble table of accounts
-	accounts := make([]DisplayAccount, len(config.Accounts))
+var OTRWhitespaceTag = append(OTRWhitespaceTagStart, OTRWhiteSpaceTagV2...)
 
-	// Pull accounts from config file
-	for i, account := range config.Accounts {
-		enabled := account.Enabled
-		acctName := strings.Join([]string{account.Name, account.Domain}, "@")
-		accounts[i] = DisplayAccount{enabled, acctName}
-	}	
+func stripHTML(msg []byte) (out []byte) {
+	z := html.NewTokenizer(bytes.NewReader(msg))
 
-	// Populate table
-	accountTable := ui.NewTable(reflect.TypeOf(accounts[0]))
-	accountTable.Lock()
-	e := accountTable.Data().(*[]DisplayAccount)
-	*e = accounts
-	accountTable.Unlock()
-
-	// Assemble button stack
-	addButton := ui.NewButton("Add")
-	modifyButton := ui.NewButton("Modify")
-	deleteButton := ui.NewButton("Delete")
-
-	// Set up channel for sending data between windows
-	done := make(chan Account)
-
-	// When "Delete" is clicked, remove selected account
-	deleteButton.OnClicked(func() {
-		sel := accountTable.Selected()
-		cfgAccts := config.Accounts
-
-		// Delete the account from the config file
-		config.Accounts = append(cfgAccts[:sel], cfgAccts[sel+1:]...)
-		config.Save()
-
-		// Delete the account from the table by building a new slice omitting it
-		accountTable.Lock()
-		tblAccts := accountTable.Data().(*[]DisplayAccount)
-		aa := *tblAccts
-		*tblAccts = append(aa[:sel], aa[sel+1:]...)
-		accountTable.Unlock()
-	})
-
-	// When "Modify" is clicked, present selected account for editing
-	modifyButton.OnClicked(func() {
-		sel := accountTable.Selected()
-		cfgAccts := config.Accounts
-
-		if sel < 0 {
-			return
+loop:
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.TextToken:
+			out = append(out, z.Text()...)
+		case html.ErrorToken:
+			if err := z.Err(); err != nil && err != io.EOF {
+				out = msg
+				return
+			}
+			break loop
 		}
-
-		// Retrieve selected account
-		acct := cfgAccts[sel]
-		
-		// Open window to edit selected account
-		editAccount(acct, done)
-
-		go func() {
-			// Get edited account
-			editedAcct := <- done
-
-			// Update the account
-			config.Accounts[sel] = editedAcct
-			config.Save()
-
-			// Update the accounts list
-			accounts := make([]DisplayAccount, len(config.Accounts))
-			
-			for i, account := range config.Accounts {
-				acctName := strings.Join([]string{account.Name, account.Domain}, "@")
-				accounts[i] = DisplayAccount{account.Enabled, acctName}
-			}	
-
-			// Update accountTable
-			accountTable.Lock()
-			e := accountTable.Data().(*[]DisplayAccount)
-			*e = accounts
-			accountTable.Unlock()
-		}()
-	})
-
-	// When "Add" is clicked, open window to enter new account info
-	addButton.OnClicked(func() {
-		// Create an empty Account to pass to editAccount
-		var account Account;
-
-		// Open the edit window and wait for a result
-		editAccount(account, done)
-
-		// Update accounts list concurrently once we hear back from the edit window
-		go func() {
-			acct := <-done
-
-			// Add new account to config.Accounts
-			config.Accounts = append(config.Accounts, acct)
-			config.Save()
-
-			// Update the accounts list
-			accounts := make([]DisplayAccount, len(config.Accounts))
-			
-			for i, account := range config.Accounts {
-				acctName := strings.Join([]string{account.Name, account.Domain}, "@")
-				accounts[i] = DisplayAccount{account.Enabled, acctName}
-			}	
-
-			accountTable.Lock()
-			e := accountTable.Data().(*[]DisplayAccount)
-			*e = accounts
-			accountTable.Unlock()
-		}()
-	})
-
-	// Put the interface together
-	buttons := ui.NewHorizontalStack(
-		addButton,
-		modifyButton,
-		deleteButton)
-
-	accountsInterface := ui.NewVerticalStack(
-		accountTable,
-		buttons)
-
-	accountsInterface.SetStretchy(0)
-
-	// Build/display the window
-	acctWin := ui.NewWindow("Accounts", 400, 500, accountsInterface)
-
-	acctWin.OnClosing(func() bool {
-		ui.Stop()
-		return true
-	})
-
-	acctWin.Show()
-}
-
-func editAccount(account Account, done chan Account) {
-	// Initialize fields
-	enabled := ui.NewCheckbox("Enabled")
-	username := ui.NewTextField()
-	domain := ui.NewTextField()
-	password := ui.NewPasswordField()
-
-	// If this is an existing account, populate the fields from the config file
-	if len(account.Name) > 0 {
-		enabled.SetChecked(account.Enabled)
-		username.SetText(account.Name)
-		domain.SetText(account.Domain)
-		password.SetText(account.Password)		
-	} else {
-		enabled.SetChecked(false)
-		username.SetText("e.g. 'esnowden'")
-		domain.SetText("e.g. 'ccc.de'")
-		password.SetText("password1234")
 	}
 
-	// Assemble input fields
-	textFields := ui.NewVerticalStack(
-		ui.NewStandaloneLabel("Enabled"),
-		enabled,
-		ui.NewStandaloneLabel("Username"),
-		username,
-		ui.NewStandaloneLabel("Domain"),
-		domain,
-		ui.NewStandaloneLabel("Password"),		
-		password)
+	return
+}
 
-	// Assemble button stack
-	saveButton := ui.NewButton("Save")
-	cancelButton := ui.NewButton("Cancel")
+type Session struct {
+	account string
+	conn    *xmpp.Conn
+	term    *terminal.Terminal
+	roster  []xmpp.RosterEntry
+	input   Input
+	// conversations maps from a JID (without the resource) to an OTR
+	// conversation. (Note that unencrypted conversations also pass through
+	// OTR.)
+	conversations map[string]*otr.Conversation
+	// knownStates maps from a JID (without the resource) to the last known
+	// presence state of that contact. It's used to deduping presence
+	// notifications.
+	knownStates map[string]string
+	privateKey  *otr.PrivateKey
+	config      *Config
+	// lastMessageFrom is the JID (without the resource) of the contact
+	// that we last received a message from.
+	lastMessageFrom string
+	// timeouts maps from Cookies (from outstanding requests) to the
+	// absolute time when that request should timeout.
+	timeouts map[xmpp.Cookie]time.Time
+	// pendingRosterEdit, if non-nil, contains information about a pending
+	// roster edit operation.
+	pendingRosterEdit *rosterEdit
+	// pendingRosterChan is the channel over which roster edit information
+	// is received.
+	pendingRosterChan chan *rosterEdit
+	// pendingSubscribes maps JID with pending subscription requests to the
+	// ID if the iq for the reply.
+	pendingSubscribes map[string]string
+	// lastActionTime is the time at which the user last entered a command,
+	// or was last notified.
+	lastActionTime time.Time
+}
 
-	buttons := ui.NewHorizontalStack(
-		saveButton,
-		cancelButton)
+// rosterEdit contains information about a pending roster edit. Roster edits
+// occur by writing the roster to a file and inviting the user to edit the
+// file.
+type rosterEdit struct {
+	// fileName is the name of the file containing the roster information.
+	fileName string
+	// roster contains the state of the roster at the time of writing the
+	// file. It's what we diff against when reading the file.
+	roster []xmpp.RosterEntry
+	// isComplete is true if this is the result of reading an edited
+	// roster, rather than a report that the file has been written.
+	isComplete bool
+	// contents contains the edited roster, if isComplete is true.
+	contents []byte
+}
 
-	editAccountInterface := ui.NewVerticalStack(
-		textFields,
-		buttons)
+func (s *Session) readMessages(stanzaChan chan<- xmpp.Stanza) {
+	defer close(stanzaChan)
 
-	editAccountInterface.SetStretchy(0)
-
-	// Build and display window
-	editWin := ui.NewWindow("Edit Account", 400, 400, editAccountInterface)
-
-	editWin.OnClosing(func() bool {
-		return true
-	})
-
-	//Save a new account
-	saveButton.OnClicked(func() {
-		//Validate the data (TODO: use the fields' methods for this)
-		validUsername := regexp.MustCompile("[^\x00-\x20\x22\x26\x27\x2F\x3A\x3C\x3E\x40\x7F]+")
-//		validDomain := regexp.MustCompile("(\w{2,}\.\w{2,3}\.\w{2,3}|\w{2,}\.\w{2,3})$")
-
-		if strings.Trim(username.Text(), " ") == "" {
-			username.Invalid("You must enter a username")
-		} else if validUsername.MatchString(username.Text()) == false {
-			username.Invalid("Username contains invalid characters")
-//		} else if validDomain.MatchString(domain.Text()) == false {
-//			domain.Invalid("Domain contains invalid characters")
-		} else {
-			//Instantiate a new account and grow config.Accounts to add it
-			acct := Account{Enabled: enabled.Checked(), Name: username.Text(), Domain: domain.Text(), Password: password.Text()}
-			editWin.Hide()
-			done <- acct
+	for {
+		stanza, err := s.conn.Next()
+		if err != nil {
+			alert(s.term, err.Error())
+			return
 		}
-	})
-
-	cancelButton.OnClicked(func() {
-		editWin.Hide()
-	})
-
-	editWin.Show()
+		stanzaChan <- stanza
+	}
 }
 
 func main() {
-	go ui.Do(listAccounts)
-	err := ui.Go()
+	// Import the configuration
+	config := importConfig();
+
+	// TODO: encrypt/decrypt this, derp
+	password := config.Password
+
+	// TODO: add connections for other users 
+	account = config.Accounts[0]
+
+	user := account.Name
+	domain := account.Domain
+
+	var addr string
+	addrTrusted := false
+
+	if len(account.Server) > 0 && account.Port > 0 {
+		addr = fmt.Sprintf("%s:%d", account.Server, account.Port)
+		addrTrusted = true
+	} else {
+		if len(config.Proxies) > 0 {
+			alert(term, "Cannot connect via a proxy without Server and Port being set in the config file as an SRV lookup would leak information.")
+			return
+		}
+		host, port, err := xmpp.Resolve(domain)
+		if err != nil {
+			alert(term, "Failed to resolve XMPP server: "+err.Error())
+			return
+		}
+		addr = fmt.Sprintf("%s:%d", host, port)
+	}
+
+	var dialer proxy.Dialer
+	for i := len(config.Proxies) - 1; i >= 0; i-- {
+		u, err := url.Parse(config.Proxies[i])
+		if err != nil {
+			alert(term, "Failed to parse "+config.Proxies[i]+" as a URL: "+err.Error())
+			return
+		}
+		if dialer == nil {
+			dialer = proxy.Direct
+		}
+		if dialer, err = proxy.FromURL(u, dialer); err != nil {
+			alert(term, "Failed to parse "+config.Proxies[i]+" as a proxy: "+err.Error())
+			return
+		}
+	}
+
+	var certSHA256 []byte
+	if len(account.ServerCertificateSHA256) > 0 {
+		certSHA256, err = hex.DecodeString(account.ServerCertificateSHA256)
+		if err != nil {
+			alert(term, "Failed to parse ServerCertificateSHA256 (should be hex string): "+err.Error())
+			return
+		}
+		if len(certSHA256) != 32 {
+			alert(term, "ServerCertificateSHA256 is not 32 bytes long")
+			return
+		}
+	}
+	xmppConfig := &xmpp.Config{
+		Log:                     &lineLogger{term, nil},
+		Create:                  *createAccount,
+		TrustedAddress:          addrTrusted,
+		Archive:                 false,
+		ServerCertificateSHA256: certSHA256,
+	}
+
+	if len(config.RawLogFile) > 0 {
+		rawLog, err := os.OpenFile(config.RawLogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+		if err != nil {
+			alert(term, "Failed to open raw log file: "+err.Error())
+			return
+		}
+
+		lock := new(sync.Mutex)
+		in := rawLogger{
+			out:    rawLog,
+			prefix: []byte("<- "),
+			lock:   lock,
+		}
+		out := rawLogger{
+			out:    rawLog,
+			prefix: []byte("-> "),
+			lock:   lock,
+		}
+		in.other, out.other = &out, &in
+
+		xmppConfig.InLog = &in
+		xmppConfig.OutLog = &out
+
+		defer in.flush()
+		defer out.flush()
+	}
+
+	if dialer != nil {
+		info(term, "Making connection to "+addr+" via proxy")
+		if xmppConfig.Conn, err = dialer.Dial("tcp", addr); err != nil {
+			alert(term, "Failed to connect via proxy: "+err.Error())
+			return
+		}
+	}
+
+	conn, err := xmpp.Dial(addr, user, domain, password, xmppConfig)
 	if err != nil {
-		panic(err)
+		alert(term, "Failed to connect to XMPP server: "+err.Error())
+		return
+	}
+
+	s := Session{
+		account:           config.Account[0],
+		conn:              conn,
+		term:              term,
+		conversations:     make(map[string]*otr.Conversation),
+		knownStates:       make(map[string]string),
+		privateKey:        new(otr.PrivateKey),
+		config:            config,
+		pendingRosterChan: make(chan *rosterEdit),
+		pendingSubscribes: make(map[string]string),
+		lastActionTime:    time.Now(),
 	}
 }
+
